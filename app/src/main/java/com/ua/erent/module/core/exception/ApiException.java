3cece8d8016fcda68fcaaa63dcf46a590ec510b4
Xlopec <maksimolejn720@gmail.com>
2016-11-26 22:13:53 +0200

    minor

---
+++ app/src/main/java/com/ua/erent/module/core/exception/ApiException.java
@@ -0,0 +1,8 @@
+package com.ua.erent.module.core.exception;
+
+/**
+ * Created by Максим on 11/26/2016.
+ */
+
+public class ApiException extends Exception {
+}
---
+++ app/src/main/java/com/ua/erent/module/core/exception/FileUploadException.java
@@ -0,0 +1,27 @@
+package com.ua.erent.module.core.exception;
+
+/**
+ * Created by Максим on 11/26/2016.
+ */
+
+public class FileUploadException extends Exception {
+
+    public FileUploadException() {
+    }
+
+    public FileUploadException(String message) {
+        super(message);
+    }
+
+    public FileUploadException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public FileUploadException(Throwable cause) {
+        super(cause);
+    }
+
+    public FileUploadException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
